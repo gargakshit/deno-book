91da0f34390304355aa85f88c4a0a97460140579
Akshit Garg <garg.akshit@gmail.com>
2020-07-27 14:16:58 +0530

    [Book] Improve the web-server section; Add alt to the logo component

--- src/@rocketseat/gatsby-theme-docs/components/Logo.js
+++ src/@rocketseat/gatsby-theme-docs/components/Logo.js
@@ -1,3 +1,5 @@
 import React from "react";
 
-export default () => <img src="/favicon.svg" height="36px" />;
+export default () => (
+  <img src="/favicon.svg" height="36px" alt="Deno - A book" />
+);
